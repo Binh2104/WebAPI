e67f8d22d08a94ee9335a1487b6db961cea71d3a
nhubinh2104 <nhubinh2104@gmail.com>
2023-04-18 11:10:16 +0700

    add database

---
+++ QLTourDuLichAPI/QLTourDuLichAPI/Models/DiaDiemTour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QLTourDuLichAPI.Models;
+
+public partial class DiaDiemTour
+{
+    public string MaDd { get; set; } = null!;
+
+    public string ViTriAnh { get; set; } = null!;
+
+    public string MaTour { get; set; } = null!;
+
+    public virtual DiemThamQuan MaDdNavigation { get; set; } = null!;
+
+    public virtual Tour MaTourNavigation { get; set; } = null!;
+}
