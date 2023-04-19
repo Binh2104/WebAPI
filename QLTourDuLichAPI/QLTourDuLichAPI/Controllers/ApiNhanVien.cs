ecd3f7a2dca1736e632df5f0b240f18b95ff482f
nguyen van loi <nguyenvanloihd88@gmail.com>
2023-04-19 19:58:43 +0700

    xongdanhsachtintuc

---
+++ QLTourDuLichAPI/QLTourDuLichAPI/Controllers/ApiNhanVien.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QLTourDuLichAPI.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace QLTourDuLichAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiNhanVien : Controller
+    {
+        QltourdlApiContext db = new QltourdlApiContext();
+        [HttpGet]
+        public IActionResult GetAllNhanvien()
+        {
+            var query = (from a in db.NhanViens
+                         select new
+                         {
+                             a.MaNv,
+                             a.TenNv,
+                             a.GioiTinh,
+                             a.SoDienThoai,
+                             a.DiaChi
+                         });
+            var totalCount = query.Count();
+            //var pageCount = (int)Math.Ceiling((double)totalCount / pageSize);
+
+            var listNV = query
+                .ToList();
+
+            var result = new
+            {
+                TotalCount = totalCount,
+                //PageCount = pageCount,
+                Items = listNV
+            };
+
+            return Ok(result);
+
+        }
+        [HttpGet]
+        [Route("getPagination")]
+        public IActionResult GetAllNhanvienPagination([Range(1, 100)] int pageSize,
+           [Range(1, int.MaxValue)] int pageNumber)
+        {
+            var listNV = (from a in db.NhanViens
+                           select new
+                           {
+                               a.MaNv,
+                               a.TenNv,
+                               a.GioiTinh,
+                               a.SoDienThoai,
+                               a.DiaChi
+                           })
+                              .Skip((pageNumber - 1) * pageSize)
+                              .Take(pageSize)
+                              .ToList();
+            var result = new
+            {
+                Items = listNV
+            };
+            return Ok(result);
+        }
+    }
+}
---
+++ QLTourDuLichAPI/QLTourDuLichAPI/wwwroot/admin/layoutdmin/assets/js/nhansu.js
@@ -0,0 +1,84 @@
+﻿$(document).ready(function () {
+    getAllNhanvien();
+});
+
+function getAllNhanvien() {
+    $.ajax({
+        url: `https://localhost:7269/api/apinhanvien`,
+        method: 'GET',
+        contentType: 'json',
+        dataType: 'json',
+        error: function (response) {
+            console.log("error");
+        },
+        success: function (response) {
+            var count = parseInt(response.totalCount);
+            const pageNumber = 1;
+            const pageSize = 3;
+            $.ajax({
+                url: `https://localhost:7269/api/apinhanvien/getPagination?pageSize=${pageSize}&pagenumber=${pageNumber}`,
+                method: 'GET',
+                contentType: 'json',
+                dataType: 'json',
+                error: function (response) {
+                    console.log("error");
+                },
+                success: function (response) {
+                    renderTable(response);
+                    renderPagination(Math.ceil(count / pageSize), pageNumber);
+                },
+                fail: function (response) {
+                    console.log("fail");
+                }
+            });
+        },
+    })
+}
+$("#form-nhanvien").submit(function (e) {
+    e.preventDefault();
+})
+function renderPagination(totalPages, currentPage) {
+    let pagination = '';
+    for (let i = 1; i <= totalPages; i++) {
+        pagination += `<button class="btn ${i === currentPage ? 'btn-primary' : 'btn-outline-primary'}" onclick="setPage(${i})">${i}</button> `;
+    }
+    document.getElementById('pagination_nhanvien').innerHTML = pagination;
+}
+
+function setPage(pageNumber) {
+    const pageSize = 3;
+    document.getElementById('page-number').innerHTML = pageNumber;
+    $.ajax({
+        url: `https://localhost:7269/api/apinhanvien/getPagination?pageSize=${pageSize}&pagenumber=${pageNumber}`,
+        method: 'GET',
+        contentType: 'json',
+        dataType: 'json',
+        error: function (response) {
+            console.log("error");
+        },
+        success: function (response) {
+            renderTable(response);
+        },
+        fail: function (response) {
+            console.log("fail");
+        }
+    });
+}
+function renderTable(response) {
+    const len = response.items.length;
+    let table = '';
+    for (var i = 0; i < len; ++i) {
+        table = table + '<tr>';
+        table = table + '<td>' + response.items[i].maNv.trim() + '</td>';
+        table = table + '<td>' + response.items[i].tenNv.trim() + '</td>';
+        table = table + '<td>' + response.items[i].gioiTinh.trim() + '</td>';
+        table = table + '<td>' + response.items[i].soDienThoai.trim() + '</td>';
+        table = table + '<td>' + response.items[i].diaChi.trim() + '</td>';
+        
+        /* table = table + '<td>' + response.items[i].moTa.trim() + '</td>';*/
+
+        /* table = table + '<td>' + ' <button type="button" class="btn btn-gradient-info btn-rounded btn-icon" onclick="updateCauThuFill(\'' + response.items[i].cauThuId.trim() + '\')"><i class="mdi mdi-table-edit"></i></button> ' + '</td>';
+         table = table + '<td>' + ' <button type="button" class="btn btn-gradient-danger btn-rounded btn-icon" onclick="deleteCauThu(\'' + response.items[i].cauThuId.trim() + '\')"><i class="mdi mdi-delete-forever"></i></button> ' + '</td>';*/
+    }
+    document.getElementById('tbody-nhanvien').innerHTML = table;
+}
